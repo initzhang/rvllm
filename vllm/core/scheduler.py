878bfd804196904d2d8e7669fd06afdc149275fa
ZHANG Xin <sean.zhang@connect.ust.hk>
2025-03-21 15:05:23 +0800

    support ipabs: isolated relQuery execution + pabs

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -1064,6 +1064,171 @@ class Scheduler:
             num_lookahead_slots=self._get_num_lookahead_slots(
                 is_prefill=True, enable_chunking=enable_chunking))
 
+    def _schedule_prefills_ipabs(
+        self,
+        budget: SchedulingBudget,
+        curr_loras: Optional[Set[int]],
+        enable_chunking: bool = False,
+    ) -> SchedulerPrefillOutputs:
+        """Schedule sequence groups that are in prefill stage.
+
+        we prevent the prefill of next relQuery until the decode of
+        current query are finished
+
+        we avoid preemption amsp by considering running requests' max 
+        unfinished tokens, and proactively stop prefill
+        """
+
+        # obtain current relQuery id
+        running_rel_id = None
+        if self.running:
+            running_rel_id = self.running[0].rel_id
+
+        # obtain number of blocks to reserve for necessary future tokens 
+        NFT = 0
+        for sg in self.running:
+            max_output_tokens = sg.sampling_params.max_tokens
+            cur_seq = sg.get_seqs(status=SequenceStatus.RUNNING)[0]
+            cur_output_tokens = cur_seq.get_output_len()
+            NFT += (max_output_tokens - cur_output_tokens)
+        NFT_blocks = NFT // self.block_manager.block_size + 1
+
+        ignored_seq_groups: List[SequenceGroup] = []
+        seq_groups: List[ScheduledSequenceGroup] = []
+
+        waiting_queue = self.waiting
+
+        leftover_waiting_sequences: Deque[SequenceGroup] = deque()
+        while self._passed_delay(time.time()) and waiting_queue:
+            seq_group = waiting_queue[0]
+
+            # before scheduling, check whether have other relQuery is decoding
+            if running_rel_id is not None and seq_group.rel_id != running_rel_id:
+                break
+
+            waiting_seqs = seq_group.get_seqs(status=SequenceStatus.WAITING)
+            assert len(waiting_seqs) == 1, (
+                "Waiting sequence group should have only one prompt "
+                "sequence.")
+            num_new_tokens_uncached, num_new_tokens_cached = (
+                self._get_num_new_uncached_and_cached_tokens(
+                    seq_group, SequenceStatus.WAITING, enable_chunking,
+                    budget))
+            num_new_tokens = num_new_tokens_uncached + num_new_tokens_cached
+
+            if not enable_chunking:
+                num_prompt_tokens = waiting_seqs[0].get_len()
+                assert num_new_tokens == num_prompt_tokens
+
+            prompt_limit = self._get_prompt_limit(seq_group)
+            if num_new_tokens > prompt_limit:
+                logger.warning(
+                    "Input prompt (%d tokens) is too long"
+                    " and exceeds limit of %d", num_new_tokens, prompt_limit)
+                for seq in waiting_seqs:
+                    seq.status = SequenceStatus.FINISHED_IGNORED
+                ignored_seq_groups.append(seq_group)
+                waiting_queue.popleft()
+                continue
+
+            num_lookahead_slots: int = 0
+            if self.scheduler_config.is_multi_step and enable_chunking:
+                num_lookahead_slots = self._get_num_lookahead_slots(
+                    True, enable_chunking)
+
+            # before checking can_allocate, check reserved space for NFT
+            if self.block_manager.get_num_free_gpu_blocks() < NFT_blocks:
+                break
+
+            # If the sequence group cannot be allocated, stop.
+            can_allocate = self.block_manager.can_allocate(
+                seq_group, num_lookahead_slots=num_lookahead_slots)
+            if can_allocate == AllocStatus.LATER:
+                break
+            elif can_allocate == AllocStatus.NEVER:
+                logger.warning(
+                    "Input prompt (%d tokens) + lookahead slots (%d) is "
+                    "too long and exceeds the capacity of block_manager",
+                    num_new_tokens, num_lookahead_slots)
+                for seq in waiting_seqs:
+                    seq.status = SequenceStatus.FINISHED_IGNORED
+                ignored_seq_groups.append(seq_group)
+                waiting_queue.popleft()
+                continue
+
+            lora_int_id = 0
+            if self.lora_enabled:
+                lora_int_id = seq_group.lora_int_id
+                assert curr_loras is not None
+                assert self.lora_config is not None
+                if (self.lora_enabled and lora_int_id > 0
+                        and lora_int_id not in curr_loras
+                        and len(curr_loras) >= self.lora_config.max_loras):
+                    # We don't have a space for another LoRA, so
+                    # we ignore this request for now.
+                    leftover_waiting_sequences.appendleft(seq_group)
+                    waiting_queue.popleft()
+                    continue
+
+            if (budget.num_batched_tokens >=
+                    self.scheduler_config.max_num_batched_tokens):
+                # We've reached the budget limit - since there might be
+                # continuous prefills in the running queue, we should break
+                # to avoid scheduling any new prefills.
+                break
+
+            num_new_seqs = seq_group.get_max_num_running_seqs()
+            if num_new_tokens_uncached == 0 or not budget.can_schedule(
+                    num_new_tokens=num_new_tokens_uncached,
+                    num_new_seqs=num_new_seqs,
+            ):
+                break
+
+            # Can schedule this request.
+            if curr_loras is not None and lora_int_id > 0:
+                curr_loras.add(lora_int_id)
+            waiting_queue.popleft()
+            self._allocate_and_set_running(seq_group)
+
+            if enable_chunking and self.scheduler_config.is_multi_step:
+                blocks_to_copy: List[Tuple[int, int]] = []
+                # init_multi_step_from_lookahead_slots happens in append_slots
+                self._append_slots(seq_group, blocks_to_copy, enable_chunking)
+                # This assert will trip when a copy-on-write happens. This is
+                # not a concern as the very first sequence-group block
+                # allocation happens above. Still, we have the assert to
+                # catch any edge-cases.
+                assert not blocks_to_copy
+            else:
+                seq_group.init_multi_step_from_lookahead_slots(
+                    num_lookahead_slots,
+                    num_scheduler_steps=self.scheduler_config.
+                    num_scheduler_steps,
+                    is_multi_step=self.scheduler_config.is_multi_step,
+                    enable_chunking=enable_chunking)
+
+            seq_groups.append(
+                ScheduledSequenceGroup(seq_group=seq_group,
+                                       token_chunk_size=num_new_tokens))
+            budget.add_num_batched_tokens(
+                seq_group.request_id,
+                num_batched_tokens=num_new_tokens_uncached,
+                num_cached_tokens=num_new_tokens_cached,
+            )
+            budget.add_num_seqs(seq_group.request_id, num_new_seqs)
+
+        # Queue requests that couldn't be scheduled.
+        waiting_queue.extendleft(leftover_waiting_sequences)
+        if len(seq_groups) > 0:
+            self.prev_prompt = True
+
+        return SchedulerPrefillOutputs(
+            seq_groups=seq_groups,
+            ignored_seq_groups=ignored_seq_groups,
+            num_lookahead_slots=self._get_num_lookahead_slots(
+                is_prefill=True, enable_chunking=enable_chunking))
+
+
     def _schedule_prefills_pabs(
         self,
         budget: SchedulingBudget,
@@ -1237,7 +1402,6 @@ class Scheduler:
             ignored_seq_groups=ignored_seq_groups,
             num_lookahead_slots=self._get_num_lookahead_slots(
                 is_prefill=True, enable_chunking=enable_chunking))
-
 
 
     def _schedule_prefills(
@@ -1698,12 +1862,71 @@ class Scheduler:
             return self._schedule_default_old()
 
         logger.info("%"*30+"xzhanggb"+"%"*30)
-        assert self.scheduler_config.policy in ['priority_tc', 'priority_bs', 'priority_abs', 'priority_pabs']
+        assert self.scheduler_config.policy in ['priority_tc', 'priority_bs', 'priority_abs', 'priority_pabs', 'priority_ipabs']
         assert not self.lora_enabled
         assert not self.swapped
         curr_loras = None
 
-        if self.scheduler_config.policy == "priority_pabs":
+        if self.scheduler_config.policy == "priority_ipabs":
+            """
+            pabs with isolated relQuery execution: 
+            prefill of next relQuery starts after all previous relQuery's decode
+            """
+            # Include running requests to the budget.
+            budget = SchedulingBudget(
+                token_budget=self.scheduler_config.max_num_batched_tokens,
+                max_num_seqs=self.scheduler_config.max_num_seqs,
+            )
+
+            assert len(self.scheduler_config.info_prefill) == 2
+            assert len(self.scheduler_config.info_decode) == 2
+            self._update_query_priority_abs()
+
+            # Make sure we include num running seqs before scheduling prefill,
+            # so that we don't schedule beyond max_num_seqs for prefill.
+            for seq_group in self.running:
+                budget.add_num_seqs(seq_group.request_id,
+                                    seq_group.get_max_num_running_seqs())
+
+            prefills = SchedulerPrefillOutputs.create_empty()
+            running_scheduled = SchedulerRunningOutputs.create_empty()
+            swapped_in = SchedulerSwappedInOutputs.create_empty()
+
+            # prefill with preempt prevention
+            prefills = self._schedule_prefills_ipabs(budget,
+                                               curr_loras,
+                                               enable_chunking=False)
+
+
+        elif self.scheduler_config.policy == "priority_abs":
+            # Include running requests to the budget.
+            budget = SchedulingBudget(
+                token_budget=self.scheduler_config.max_num_batched_tokens,
+                max_num_seqs=self.scheduler_config.max_num_seqs,
+            )
+
+            assert len(self.scheduler_config.info_prefill) == 2
+            assert len(self.scheduler_config.info_decode) == 2
+            self._update_query_priority_abs()
+
+            # Make sure we include num running seqs before scheduling prefill,
+            # so that we don't schedule beyond max_num_seqs for prefill.
+            for seq_group in self.running:
+                budget.add_num_seqs(seq_group.request_id,
+                                    seq_group.get_max_num_running_seqs())
+
+            prefills = SchedulerPrefillOutputs.create_empty()
+            running_scheduled = SchedulerRunningOutputs.create_empty()
+            swapped_in = SchedulerSwappedInOutputs.create_empty()
+
+            # prefill without preemption
+            #prefills = self._schedule_prefills_separated(budget,
+            prefills = self._schedule_prefills(budget,
+                                               curr_loras,
+                                               enable_chunking=False)
+
+
+        elif self.scheduler_config.policy == "priority_pabs":
             """
             abs with preemption prevention
             """

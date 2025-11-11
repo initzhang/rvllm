f33435ac9510dfec891d24cce925ec704c32ba55
initzhang <xzhanggb@ust.hk>
2025-11-11 11:52:20 +0800

    add install bash script

---
+++ my_install.sh
@@ -0,0 +1,4 @@
+VLLM_PRECOMPILED_WHEEL_LOCATION=https://files.pythonhosted.org/packages/8d/cf/9b775a1a1f5fe2f6c2d321396ad41b9849de2c76fa46d78e6294ea13be91/vllm-0.7.3-cp38-abi3-manylinux1_x86_64.whl VLLM_USE_PRECOMPILED=1 pip install --editable .
+
+
+
